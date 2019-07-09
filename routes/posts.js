71c2d4788d1750adaa85b98ea79aef26a3227df6
Dagestan05 <steklofan@gmail.com>
2019-07-10 01:43:46 +0200

    added initial posts routes

---
+++ routes/posts.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET posts index /posts. */
+router.get('/', (req, res, next) => {
+  // res.render('index', { title: 'Pin Shop - Home' });
+  res.send("INDEX /posts")
+});
+
+/* GET - new post page - /posts/new. */
+router.get('/new', (req, res, next) => {
+  res.send("NEW /posts/new")
+});
+
+/* POST - create - /posts */
+router.post('/', (req, res, next) => {
+  res.send("POST(create) /")
+});
+
+/* GET - show - /posts/:id. */
+router.get('/:id', (req, res, next) => {
+  res.send("SHOW /posts/:id")
+});
+
+/* GET - edit - /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+  res.send("EDIT /posts/:id/edit")
+});
+
+/* PUT - posts update - /posts/:id */
+router.put('/:id', (req, res, next) => {
+  res.send("PUT /posts/:id")
+});
+
+/* DELETE - posts destroy - /posts/:id */
+router.delete('/:id', (req, res, next) => {
+  res.send(" DELETE /posts/:id")
+});
+
+
+module.exports = router;
+
+
+// GET index        /posts
+// GET new          /posts/new
+// POST create      /posts
+// GET show         /posts/:id
+// GET edit         /posts/:id/edit
+// PUT update       /posts/:id
+// DELETE update    /posts/:id
