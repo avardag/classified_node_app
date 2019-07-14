27e38ab88b7ae75911b672177d5b3d126a752eec
Dagestan05 <steklofan@gmail.com>
2019-07-14 23:54:32 +0200

    add multiple imges upload w/ multer & cloudinary

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require("../models/post");
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: 'micober',
+  api_key: '945373752449394',
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
   //Posts index : GET /posts
@@ -12,6 +18,16 @@ module.exports = {
   },
   /* POST - create - /posts */
   async postCreate(req, res, next){
+    req.body.post.images=[]; //empty array to be filled with uploads
+    // iterate over req.files & upload 1 by 1
+    for (const file of req.files) {
+      let image = await cloudinary.v2.uploader.upload(file.path);
+      // fill req.body.post.images w/ urls of uploaded images
+      req.body.post.images.push({
+        url: image.secure_url,
+        public_id: image.public_id
+      })
+    }
     //use req.body to create new post
     let newPost = await Post.create(req.body.post);
     res.redirect(`/posts/${newPost.id}`)
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({'dest': 'uploads/'}); //multer destination for imges
+
 const { asyncErrorHandler } = require("../middleware/users")
 const { 
     postIndex, 
@@ -18,7 +21,8 @@ router.get('/', asyncErrorHandler(postIndex));
 router.get('/new', postNew);
 
 /* POST - create - /posts */
-router.post('/', asyncErrorHandler(postCreate) );
+// upload.array('images', 4) -> 'images' : name of field, 4: maxNum of upload
+router.post('/', upload.array('images', 4), asyncErrorHandler(postCreate) );
 
 /* GET - show - /posts/:id. */
 router.get('/:id', asyncErrorHandler(postShow));
